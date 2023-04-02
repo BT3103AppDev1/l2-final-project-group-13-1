467f71e1dbe567d79408a42bff0d1d3eeb2b60cc
bryan <bryantan365@gmail.com>
2023-04-02 16:51:43 +0800

    completed

---
+++ src/components/EditDetails.vue
@@ -0,0 +1,190 @@
+<template>
+
+    <div class = "customer-edit-details">
+        <div class = "edit-details">
+            <span class="poppins-bold-black-20px">Edit your details<br></span>
+        </div>
+
+        <div class = "name-details">
+            <div class = "name-entry">
+                <span class="roboto-medium-flamingo">Enter your name<br></span>
+            </div>
+            <div class = "name-textbox">
+                <input type = "text" size = 112 placeholder="  amy" >
+                
+            </div>
+        </div>
+        
+        <div class = "email-details">
+            <div class = "email-entry">
+                <span class="poppins-bold-black-10px">Enter your email address<br></span>
+            </div>
+            <div class = "email-textbox">
+                <input type = "text" size = 112 placeholder="  amy380@gmail.com">
+            </div>
+        </div>
+
+        <div class = "number-details">
+            <div class = "number-entry">
+                <span class="poppins-bold-black-10px">Enter your 8-digit phone number (+65 numbers only)<br></span>
+            </div>
+            <div class = "number-textbox">
+                <input type = "text" size = 112 placeholder="  98473825">
+            </div>
+        </div>
+
+        <div class = "password-details">
+            <div class = "password-entry">
+                <span class="poppins-bold-black-10px">Enter your password<br></span>
+            </div>
+            <div class = "password-textbox">
+                <input type = "text" size = 112 placeholder="  ********">
+            </div>
+        </div>
+
+        <div class = "submit-options">
+            <button v-on:click="">Submit</button> 
+        </div>
+
+        <div class = "back-options">
+            <button v-on:click="">Back</button> 
+        </div>
+
+    </div>
+
+
+
+</template>
+
+<style>
+    /* For Page Body */
+    .customer-edit-details {
+        align-items: center;
+        background-color: var(--concrete);;
+        margin-top: 0px;
+        display: flex;
+        height: 500px;
+        margin-top: 20px;
+        width: 900px;
+    }
+
+    .edit-details {
+        display: flex;
+        margin-top: -450px;
+        margin-left: 320px;
+        height: 20px;
+        min-width: 300px;
+        padding: 6px 23px;
+        position: center;
+    }
+
+    .name-details {
+        margin-top: 150px;
+    }
+
+    .name-entry {
+        margin-top: -260px;
+        margin-left: -570px;
+        height: 30px
+    }
+
+    .name-textbox {
+        text-align: left;
+        margin-left: -570px;
+    }
+
+
+    textarea, input {
+        border-radius: 10px;
+        height: 35px;
+    }
+
+    .email-details {
+        margin-top: 600px;
+        margin-left: -800px;
+    }
+
+    .email-entry {
+        margin-top: -400px;
+        margin-left: 20px;
+        height: 30px;
+    }
+
+    .email-textbox {
+        height: 100px;
+        text-align: left;
+        margin-left: 20px;
+    }
+
+    .number-details {
+        margin-top: 740px;
+        margin-left: -800px;
+    }
+
+    .number-entry {
+        margin-top: -390px;
+        margin-left: 20px;
+        height: 30px;
+    }
+
+    .number-textbox {
+        height: 100px;
+        text-align: left;
+        margin-left: 20px;
+    }
+
+    .password-details {
+        margin-top: 920px;
+        margin-left: -800px;
+    }
+
+    .password-entry {
+        margin-top: -400px;
+        margin-left: 20px;
+        height:30px;
+    }
+
+    .password-textbox {
+        height: 100px;
+        text-align: left;
+        margin-left: 20px;
+    }
+
+    .submit-options {
+        margin-top: 370px;
+        margin-left: -650px;
+        font-family: 'Arial';
+        width: 250px;
+        background-color: orangered;
+        border: none;
+        border-radius: 20px;
+        font-size: 200px ;
+        color: white;
+        cursor: pointer;
+        font-size: 20px;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+    }
+
+    .back-options {
+        margin-top: 370px;
+        margin-left: 50px;
+        font-family: 'Arial';
+        width: 250px;
+        background-color: orange;
+        border: none;
+        border-radius: 20px;
+        font-size: 200px ;
+        color: white;
+        cursor: pointer;
+        font-size: 20px;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+    }
+
+    
+    
+    
+</style>
---
+++ src/components/Topup.vue
@@ -0,0 +1,162 @@
+<template>
+    <div class = "wallet-balance">
+        <div class = "teoheng-wallet">
+            <span class="poppins-bold-black-20px">Your TEOHENG Wallet Balance<br></span>
+        </div>
+        <div class = "value">
+            <span class="poppins-bold-black-10px">$20.00<br></span>
+        </div>
+    </div>
+
+    <div class = "Top-Up">
+        <div class = "Top-Up-Details">
+            <label>
+            <span class="poppins-bold-black-20px">Top-Up Details<br></span>
+            </label>
+        </div>
+        <div class = "Amount-Top-Up">
+            <label>
+            <span class="poppins-bold-black-10px">Amount To Top Up<br></span>
+            <input type = "text" size = 112 placeholder="  100">
+            </label>
+        </div>
+        <div class = "Payment">
+            <label>
+            <span class="poppins-bold-black-10px">Payment Method<br></span>
+            </label>
+            <div class="rectangle">
+                <p>Only Paylah or Paynow</p>
+            </div>
+        </div>
+    
+        <div class = "Top-Up-options">
+            <button v-on:click="">Top-Up</button> 
+        </div>
+
+        <div class = "back-options">
+            <button v-on:click="">Back</button> 
+        </div>
+    </div>
+    
+</template>
+
+
+<style>
+    textarea, input {
+        border-radius: 10px;
+        height: 35px;
+    }
+    .wallet-balance {
+        align-items: top;
+        background-color: #F5F5F5;
+        margin-top: 0px;
+        display: flex;
+        height: 100px;
+        margin-top: 20px;
+        width: 1000px;
+    }
+
+    .teoheng-wallet{
+        margin-left:365px;
+    }
+
+    .value{
+        margin-left: -180px;
+        margin-top: 50px;
+    }
+
+    .Top-Up {
+        align-items: top;
+        background-color: #EEEEEE;
+        margin-top: 10px;
+        display: flex;
+        height: 450px;
+        margin-top: 20px;
+        width: 1000px;
+    }
+
+    .Top-Up-Details{
+        margin-top: 30px;
+        margin-left: 450px;
+        width: 160px;
+    }
+
+    .Amount-Top-Up{
+        margin-top: 150px;
+        margin-left:-500px;
+
+    }
+
+    .Payment{
+        margin-top: 240px;
+        margin-left:-780px;
+        
+    }
+
+    .payment-methods {
+        align-items: center;
+        background-color: white;
+        border-radius: 10px;
+        display: flex;
+        gap: 8px;
+        justify-content: left;
+        padding: 7px 18px;
+        position: relative;
+        width: 1000px;
+        margin-top: 270px;
+        margin-left:860px;
+        height: 40px;
+    }
+
+    .rectangle {
+        background-color: white;
+        border-radius: 10px;
+        color: light black;
+        padding: 10px 20px;
+        height: 35px;
+        width: 780px;
+        border: 2px solid black;
+    }
+
+    .rectangle p {
+        margin: 0;
+    }
+
+    .Top-Up-options {
+        margin-top: 400px;
+        margin-left: -700px;
+        font-family: 'Arial';
+        width: 200px;
+        background-color: orangered;
+        border: none;
+        border-radius: 20px;
+        font-size: 200px ;
+        color: white;
+        cursor: pointer;
+        font-size: 20px;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+    }
+
+    .back-options {
+        margin-top: 400px;
+        margin-left: 150px;
+        font-family: 'Arial';
+        width: 250px;
+        background-color: orange;
+        border: none;
+        border-radius: 20px;
+        font-size: 200px ;
+        color: white;
+        cursor: pointer;
+        font-size: 20px;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+    }
+
+    
+
+
+</style>
