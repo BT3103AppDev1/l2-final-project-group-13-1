2c295f8685e2cf6c0284ae418b214e52dbff0c15
Vina Tan <e0775349@u.nus.edu>
2023-04-07 16:38:23 +0800

    updated routers for customer confirm, payment page

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import BookingSystemPage from '../views/BookingSystemPage.vue'
 import BookingSystemSearchResultsPage from '../views/BookingSystemSearchResultsPage.vue'
+import CustomerConfirmationPage from '../views/CustomerConfirmationPage.vue'
+import PaymentMethodCustomersPage from '../views/PaymentMethodCustomersPage.vue'
 
 const routes = [
     {
@@ -12,6 +14,16 @@ const routes = [
         path: '/booking-system-search-results-page',
         name: 'BookingSystemSearchResultsPage',
         component: BookingSystemSearchResultsPage
+    },
+    {
+        path: '/customer-confirmation-page',
+        name: 'CustomerConfirmationPage',
+        component: CustomerConfirmationPage
+    },
+    {
+        path: '/customer-payment-page',
+        name: 'PaymentMethodCustomersPage',
+        component: PaymentMethodCustomersPage
     },
 ]
 const router = createRouter({
---
+++ src/views/CustomerConfirmationPage.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container-center-horizontal">
+        <div class="booking-system-page">
+            <desktop-header/>
+            <desktop-header-welcome/>
+            <customer-confirmation/>
+            <desktop-footer/>
+        </div>
+    </div>
+</template>
+
+<script>
+import DesktopHeader from '../components/DesktopHeader.vue';
+import DesktopHeaderWelcome from '../components/DesktopHeaderWelcome.vue';
+import DesktopFooter from '../components/DesktopFooter.vue';
+import CustomerConfirmation from '../components/CustomerConfirmation.vue';
+    export default {
+        name: "CustomerConfirmationPage",
+        components: {
+            DesktopHeader,
+            DesktopHeaderWelcome,
+            CustomerConfirmation,
+            DesktopFooter,
+        },
+    }
+</script>
+
+
+
+<style>
+
+    body {
+        margin: 0;
+        padding: 0;
+        left: -35px;
+        position: absolute;
+    }
+
+    .booking-system-page {
+        align-items: center;
+        background-color: var(--white);
+        border: 1px none;
+        display: flex;
+        flex-direction: column;
+        height: 1276px;
+        overflow: hidden;
+        position: relative;
+        width: 1440px;
+    }
+
+</style>
