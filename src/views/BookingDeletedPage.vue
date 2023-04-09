b5379795dc666d005cb8211a513eb1ee481fe15d
LohYangXian <e0726433@u.nus.edu>
2023-04-09 14:22:10 +0800

    Made CSS Fixes to more urgent matters

--- src/views/BookingDeletedPage.vue
+++ src/views/BookingDeletedPage.vue
@@ -32,8 +32,8 @@ import DesktopFooter from '../components/DesktopFooter.vue';
     body {
         margin: 0;
         padding: 0;
-        left: -35px;
-        position: absolute;
+        /* left: -35px; */
+        /* position: absolute; */
     }
 
     .booking-deleted-page {
