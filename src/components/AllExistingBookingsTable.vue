c32538cb02c04721348179fd9bbc5f9c89398afb
altakyan <altakyan@gmail.com>
2023-04-10 17:39:13 +0800

    Connected firebase to website

--- src/components/AllExistingBookingsTable.vue
+++ src/components/AllExistingBookingsTable.vue
@@ -12,19 +12,117 @@
           <th>Room Type</th>
         </tr>
       </thead>
-      <tbody>
-        <td>1</td>
-        <td>123</td>
-        <td>Raffles Hall</td>
-        <td>29 Mar 23</td>
-        <td>10:00-12:00</td>
-        <td>3</td>
-        <td>Medium</td>
-        <td> <router-link to="/ExistingBookingDetails">More Details</router-link> </td>
-      </tbody>
       </table>
     </div>
   </template>
+
+<script>
+import firebaseApp from '../firebase.js'
+import { getFirestore } from 'firebase/firestore'
+import { collection, getDocs, doc, deleteDoc } from 'firebase/firestore'
+const db = getFirestore(firebaseApp)
+export default {
+   data() {
+      return {collectedData: []}
+   },
+
+
+   async mounted() {
+    let value = await getDocs(collection(db, "Bookings"))
+    let index = 1
+    value.forEach((d) => {
+          let documentData = d.data()
+          let userID =  documentData.userID
+          if (userID == "E2HEyNJWY5maFTIu6YEs") {
+            let name = documentData.name
+            let branchID = documentData.branchID
+            let date = documentData.date
+            let time = documentData.startTime
+            let numberOfPax = documentData.numberOfPax
+            let roomType = documentData.roomType
+            //  create rows and cells in the table
+            let table = document.getElementById('table')
+            let row = table.insertRow(index)
+            let cell1 = row.insertCell(0)
+            let cell2 = row.insertCell(1)
+            let cell3 = row.insertCell(2)
+            let cell4 = row.insertCell(3)
+            let cell5 = row.insertCell(4)
+            let cell6 = row.insertCell(5)
+            let cell7 = row.insertCell(6)
+            let cell8 = row.insertCell(7)
+            cell1.innerHTML = index
+            cell2.innerHTML = name
+            cell3.innerHTML = branchID
+            cell4.innerHTML = date
+            cell5.innerHTML = time
+            cell6.innerHTML = numberOfPax
+            cell7.innerHTML = roomType
+            cell8.innerHTML = "More Details"
+            index += 1
+          }
+        })
+   }
+}
+
+
+
+   /* async function :display(userID),{
+        let allDocuments = await getDocs(collection(db, 'Bookings'))
+        let index = 1
+        let tb = document.getElementById('table')
+        while (tb.rows.length > 1) {
+          tb.deleteRow(1)
+        }
+        allDocuments.forEach((docs) => {
+          let documentData = docs.data()
+          let userID = documentData.userID
+          if (userID != "E2HEyNJWY5maFTIu6YEs") {
+            continue;}
+          let name = documentData.name
+          let branchID = documentData.branchID
+          let date = documentData.date
+          let time = documentData.startTime
+          let numberOfPax = documentData.numberOfPax
+          let roomType = documentData.roomType
+          //  create rows and cells in the table
+          let table = document.getElementById('table')
+          let row = table.insertRow(index)
+          let cell1 = row.insertCell(0)
+          let cell2 = row.insertCell(1)
+          let cell3 = row.insertCell(2)
+          let cell4 = row.insertCell(3)
+          let cell5 = row.insertCell(4)
+          let cell6 = row.insertCell(5)
+          let cell7 = row.insertCell(6)
+          let cell8 = row.insertCell(7)
+          cell1.innerHTML = index
+          cell2.innerHTML = name
+          cell3.innerHTML = branchID
+          cell4.innerHTML = date
+          cell5.innerHTML = time
+          cell6.innerHTML = numberOfPax
+          cell7.innerHTML = roomType
+          cell8.innerHTML = "More Details"
+          // creating elements and giving class names through javascript
+          cell7.className = 'profits'
+          let deleteButton = document.createElement('button')
+          deleteButton.id = String(coin)
+          deleteButton.className = 'bwt'
+          deleteButton.innerHTML = 'Delete'
+          cell8.appendChild(deleteButton)
+          deleteButton.onclick = function () {
+            deleteInstrument(coin, useremail)
+          }
+          index += 1
+        }) */
+  
+
+
+
+
+</script>
+
 
 <style>
 table {
@@ -46,3 +144,4 @@ td {
   border: 1px solid black;
 }
 </style>
+
