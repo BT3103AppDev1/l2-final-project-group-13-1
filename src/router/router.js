3eb174591325d74c24e9d8a4faeef11c98d22180
Vina Tan <e0775349@u.nus.edu>
2023-04-17 03:43:10 +0800

    updated make a booking route

--- src/router/router.js
+++ src/router/router.js
@@ -66,7 +66,7 @@ const routes = [
         component: WalletDetailsPage
     },
     {
-        path: '/customer-booking-system-page',
+        path: '/booking-system-page',
         name: 'BookingSystemPage',
         component: BookingSystemPage
     },
